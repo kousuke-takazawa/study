2ad5fbb19cba56697360da545cd8abb71d3b34b1
kousuke-takazawa <28259155+kousuke-takazawa@users.noreply.github.com>
2017-09-18 20:29:02 +0900

    エラーハンドリング追加

--- 60_ジョブ/market.py
+++ 60_ジョブ/market.py
@@ -2,11 +2,18 @@
 import json
 
 # zaifトークンの価格取得
-response = requests.get('https://api.zaif.jp/api/1/last_price/zaif_jpy')
-if response.status_code != 200:
-    raise Exception('return status code is {}'.format(response.status_code))
-zaif_jpy = json.loads(response.text)
-zaif_price = zaif_jpy['last_price']
+def getZaif():
+    try:
+        response = requests.get('https://api.zaif.jp/api/1/last_price/zaif_jpy')
+        if response.status_code != 200:
+            raise Exception('return status code is {}'.format(response.status_code))
+        zaif_jpy = json.loads(response.text)
+        zaif_price = str(zaif_jpy['last_price'])
+
+    except:
+        zaif_price = 'N/A'
+    
+    return zaif_price
 
 # ビットコインの価格取得
 response = requests.get('https://api.zaif.jp/api/1/last_price/btc_jpy')
@@ -41,29 +48,42 @@ import urllib.parse
 from pprint import pprint
 
 # USDJPY用
-params_usd = {
-    "q": 'select * from yahoo.finance.xchange where pair in ("USDJPY")',
-    "format": "json",
-    "env": "store://datatables.org/alltableswithkeys"
-}
+def getUsdJpy():
+    try:
+        params_usd = {
+            "q": 'select * from yahoo.finance.xchange where pair in ("USDJPY")',
+            "format": "json",
+            "env": "store://datatables.org/alltableswithkeys"
+        }
 
-url_usd = "https://query.yahooapis.com/v1/public/yql?" + urllib.parse.urlencode(params_usd)
-res_usd = urlopen(url_usd)
-result_usd = json.loads(res_usd.read().decode('utf-8'))
-usdjpy = result_usd['query']['results']['rate']['Ask']
+        url_usd = "https://query.yahooapis.com/v1/public/yql?" + urllib.parse.urlencode(params_usd)
+        res_usd = urlopen(url_usd)
+        result_usd = json.loads(res_usd.read().decode('utf-8'))
+        usdjpy = str(result_usd['query']['results']['rate']['Ask'])
 
+    except:
+        usdjpy = 'N/A'
+    
+    return usdjpy
 
 # AUDJPY用
-params_aud = {
-    "q": 'select * from yahoo.finance.xchange where pair in ("AUDJPY")',
-    "format": "json",
-    "env": "store://datatables.org/alltableswithkeys"
-}
+def getAudJpy():
+    try:
+        params_aud = {
+            "q": 'select * from yahoo.finance.xchange where pair in ("AUDJPY")',
+            "format": "json",
+            "env": "store://datatables.org/alltableswithkeys"
+        }
 
-url_aud = "https://query.yahooapis.com/v1/public/yql?" + urllib.parse.urlencode(params_aud)
-res_aud = urlopen(url_aud)
-result_aud = json.loads(res_aud.read().decode('utf-8'))
-audjpy = result_aud['query']['results']['rate']['Ask']
+        url_aud = "https://query.yahooapis.com/v1/public/yql?" + urllib.parse.urlencode(params_aud)
+        res_aud = urlopen(url_aud)
+        result_aud = json.loads(res_aud.read().decode('utf-8'))
+        audjpy = str(result_aud['query']['results']['rate']['Ask'])
+    
+    except:
+        audjpy = 'N/A'
+        
+    return audjpy
 
 # トークンとAPIを設定
 line_notify_token = 'o4kRLJltbxWg3xgVvmg2XDq7JilBtBmShFDegPZsCKp'
@@ -71,11 +91,11 @@ line_notify_api = 'https://notify-api.line.me/api/notify'
 
 # メッセージを作成
 message = '\n' \
-    + 'zaif:' + str(zaif_price) + '\n' \
+    + 'zaif:' + getZaif() + '\n' \
     + 'btc:' + str(btc_price) + '\n' \
     + '日経平均:' + nikkei + '\n' \
-    + 'ドル円:' + usdjpy + '\n' \
-    + '豪ドル円:' + audjpy + '\n' \
+    + 'ドル円:' + getUsdJpy() + '\n' \
+    + '豪ドル円:' + getAudJpy() + '\n' \
     + '金:' + gold\
 
 
